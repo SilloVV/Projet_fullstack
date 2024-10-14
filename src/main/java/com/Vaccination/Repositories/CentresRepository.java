5fa85081f62ae5f0aa9dc45ba812eacac2033c81
Wassil <wnakib21@gmail.com>
2024-10-14 13:15:00 +0200

    ajout des repositories

---
+++ src/main/java/com/Vaccination/Repositories/CentresRepository.java
@@ -0,0 +1,8 @@
+package com.Vaccination.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.Vaccination.entities.centres;
+
+
+public interface CentresRepository extends JpaRepository<centres, Integer> {
+}
---
+++ src/main/java/com/Vaccination/Repositories/EmployesRepository.java
@@ -0,0 +1,11 @@
+package com.Vaccination.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.Vaccination.entities.employes;
+
+
+
+public interface EmployesRepository extends JpaRepository<employes, Integer> {
+
+    
+}
